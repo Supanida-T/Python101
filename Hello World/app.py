e15c0f49f7ed415cfe9b3ff78107be9af4e2dbe8
Supanida-T <supanida@welovebug.biz>
2024-04-22 10:38:34 +0700

    [Added]: เพิ่มไฟล์ app.py

---
+++ Hello World/app.py
@@ -0,0 +1,25 @@
+#Basic print Command
+print ("Tonmint")
+print (' o---- ')
+print (' ||||')
+print ('*' * 10)
+
+# #Basic Variables
+# price = 10
+# rating = 4.9
+# name = 'Tonmint'
+# is_published = True
+# print(price)
+
+#Mini Assignment
+# full_name = 'John Smith'
+# age = 20 
+# is_new_patient = True
+
+# print(full_name)
+# print(age)
+# print(is_new_patient) 
+
+#Basic function
+name = input('What is your name? ')
+print('Hi ' + name)
