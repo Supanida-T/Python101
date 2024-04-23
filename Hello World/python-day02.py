6dab364a5dbac4db70ba22735fb8bfc929c2fd31
Supanida-T <supanida@welovebug.biz>
2024-04-23 11:40:52 +0700

    [Updated]: เพิ่ม List Methods  ในไฟล์ python-day02.py

--- Hello World/python-day02.py
+++ Hello World/python-day02.py
@@ -86,13 +86,20 @@
 # print(max)
 
 #2D Lists
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 # matrix[0] [1] = 20
 # print(matrix[0] [1])
 # for row in matrix:
 #     for item in row:
 #         print(item)
+
+#List Methods
+# numbers = [5, 2, 1, 7,4]
+# numbers.append(5)
+# numbers.pop(0)
+# numbers.index()
+# print(numbers)
