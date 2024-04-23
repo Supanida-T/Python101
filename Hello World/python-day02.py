d55bb1daabaf46dc07d26dcfad10dec81d367674
Supanida-T <supanida@welovebug.biz>
2024-04-23 17:57:51 +0700

    [Updated]: เพิ่ม Key Argument ในไฟล์ python-day02.py

--- Hello World/python-day02.py
+++ Hello World/python-day02.py
@@ -152,3 +152,12 @@
 # print("Start")
 # greet_user("John")
 # print("Finish")
+
+#Keyword Argument
+# def greet_user(first_name, last_name):
+#     print(f'Hi {first_name} {last_name}')
+#     print('Welcome aboard')
+    
+# print("Start")
+# greet_user(last_name="Smith", first_name="John")
+# print("Finish")
