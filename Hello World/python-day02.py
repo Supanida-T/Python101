b3b4520ba325c947a3db4428e4a9b1739a3eddac
Supanida-T <supanida@welovebug.biz>
2024-04-23 11:06:36 +0700

    [Updated]: เพิ่ม Car Game ในไฟล์ python-day02.py

--- Hello World/python-day02.py
+++ Hello World/python-day02.py
@@ -17,3 +17,29 @@
 #         break
 # else: 
 #     print('Sorry you false')
+
+#Car Game
+# command = ""
+# started = False
+# while True:
+#     command = input("> ")
+#     if command.lower() == "start":
+#         if started:
+#             print("Car is already started")
+#         else:
+#             started = True
+#         print("Car started...")
+#     elif command.lower() == "stop":
+#         if not started:
+#             print("Car is already stopped!")
+#         print("Car stopped.")
+#     elif command == "help":
+#         print("""
+#             start - to start the car
+#             stop - to stop the car
+#             quit - to quit
+#             """)
+#     elif command == "quit":
+#         break
+#     else:
+#         print("Sorry, I don't understad the command")
