6c551112b337638a1479bb0e8fb5d9e76a7433a5
Supanida-T <supanida@welovebug.biz>
2024-05-02 10:28:00 +0700

    [Updated]: เพิ่ม Creating a Reusable Function ในไฟล์ python-day03.py

--- Hello World/python-day03.py
+++ Hello World/python-day03.py
@@ -5,3 +5,17 @@
 
 # print(square(3))
 
+#Creating a Reusable Function
+# def emoji_converter(message):
+#     words = message.split(" ")
+#     emoji = {
+#         ":)": "😀",
+#         ":(": "😞"
+#     }
+#     output = ""
+#     for word in words:
+#         output += emoji.get(word, word) + " "
+#     return output
+
+# message = input(">")
+# print(emoji_converter(message))
